f94e8e2980beb0830486adc49dbb62b5b03fc73f
Ethan Brown <eb934053@gmail.com>
2025-04-25 13:53:25 -0500

    Created POST request to retrieve user credentials

--- src/authentication/login.jsx
+++ src/authentication/login.jsx
@@ -1,12 +1,42 @@
 import { useRef } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export const Login = () => {
   const email = useRef();
   const password = useRef();
+  const navigate = useNavigate();
+
+  const loginUser = async (event) => {
+    // Prevent refresh on form submission
+    event.preventDefault()
+
+    // Create object to send to server
+    const loginFormObject = {
+        email: email.current.value,
+        password: password.current.value
+    }
+
+    // Makes POST request to API to retrieve user data
+    const response = await fetch("http://localhost:8000/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(loginFormObject)
+    })
+
+    // Serialize response into JavaScript usable data
+    const storeAsUser = await response.json()
+    
+    if (!response.ok) {
+        throw new Error("There was an issue registering your account")
+    }
+    localStorage.setItem("store_token", storeAsUser.token)
+    navigate("/")
+}
 
   return (
-    <form className="login-form">
+    <form className="login-form" type="submit" onSubmit={loginUser}>
       <h1>Login</h1>
       <fieldset className="login-fields">
         <h3>All fields are required</h3>
@@ -16,6 +46,8 @@ export const Login = () => {
         <label>Password</label> <input type="password" ref={password} />{" "}
         <br />
         New Member? <Link to="/register">Register here!</Link>
+        <br/>
+        <button className="login-button" id="login">Login</button>
       </fieldset>
     </form>
   );
