611ad1edab8ae15f4cec3b4655d9bb332ba8b7bd
Ethan Brown <eb934053@gmail.com>
2025-04-28 15:39:52 -0500

    Mapped expanded types field on cooler list items

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,18 @@
 import { useEffect, useState } from "react"
-import { useNavigate } from "react-router-dom"
-import { createCooler } from "../services/CoolerServices/coolers"
+import { Link, useNavigate } from "react-router-dom"
+import { createCooler, getStoreCoolers } from "../services/CoolerServices/coolers"
 
 export const Home = ({ token }) => {
+    const [coolers, setCoolers] = useState([])
     const [cooler, setCooler] = useState({})
     const navigate = useNavigate();
+
+    useEffect(() => {
+        if("token" in token) {
+            // This useEffect will be responsible for GETting a store's set of coolers
+            getStoreCoolers(token).then((coolersArray) => setCoolers(coolersArray))
+        }
+    }, [token])
 
     const handleCoolerCreation = () => {
         // I will create a new cooler in the API before the user navigates
@@ -24,6 +32,22 @@ export const Home = ({ token }) => {
         <article className="home-page">
             <section className="coolers">
                 {/* I will map my cooler arrays here */}
+                <ul>
+
+                {coolers.map((cooler, index) => {
+                    return (
+                    <li key={cooler.id}>
+                        <Link to={`${cooler.id}`}>Cooler #{index + 1}</Link>{" "} | {" "}
+                        Total Capacity {cooler.total_capacity} units <br />
+                        Contains: | {cooler.cooler_types.map((type) => {
+                            return (
+                                <> {type.name} | </>
+                            )
+                        })} 
+                    </li>
+                    )
+                })}
+                </ul>
                 {/* Cooler button will navigate user to the cooler creation form */}
                 <button className="new-cooler" id="cooler" onClick={handleCoolerCreation}>New Cooler</button>
             </section>
